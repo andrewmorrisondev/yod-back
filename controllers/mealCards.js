0459dcddee4168f32be72c587c2415b46272aaec
Andrew Morrison <morrison.andrew422@gmail.com>
2023-06-07 12:54:45 -0400

    add addPhoto

--- controllers/mealCards.js
+++ controllers/mealCards.js
@@ -1,3 +1,4 @@
+const { cloudinary } = require('cloudinary')
 const { MealCard } = require('../models')
 
 async function create(req, res) {
@@ -49,9 +50,34 @@ async function destroy(req, res) {
   }
 }
 
+function addPhoto(req, res) {
+  const imageFile = req.files.photo.path
+  MealCard.findByPk(req.params.id)
+    .then(mealCard => {
+      cloudinary.uploader.upload(imageFile, { tags: 'mealCard' })
+        .then(image => {
+          mealCard.photo = image.url
+          mealCard.save()
+            .then((mealCard) => {
+              res.status(200).json(mealCard.photo)
+            })
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(500).json({ error: err })
+        })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
+}
+
+
 module.exports = {
   create,
   index,
   update,
-  destroy
+  destroy,
+  addPhoto
 }
