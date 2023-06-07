de62e63d3e2e43e3a0fbbea0d2ee320647746a60
Andrew Morrison <morrison.andrew422@gmail.com>
2023-06-07 18:53:00 -0400

    fix model

--- models/mealcard.js
+++ models/mealcard.js
@@ -17,7 +17,6 @@ module.exports = (sequelize, DataTypes) => {
   MealCard.init({
     photo: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     about: {
       type: DataTypes.STRING,
